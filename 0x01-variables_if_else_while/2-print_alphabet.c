7bb0655fef6d43b0cbd6d24d8028a7d29ff4e5b7
vicedo19 <victorugochukwuka@gmail.com>
2023-06-16 14:00:55 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+
+	}
+	return (0);
+}
