842de356526ef2e2ac5c70f4ca3e6ada5f830b1e
vicedo19 <victorugochukwuka@gmail.com>
2023-06-16 01:26:59 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char ch="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	putchar(ch);
+	return (1);
+}
