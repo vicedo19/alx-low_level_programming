6dc9a9cd2b38ad11fd9788098dd1a1161e10a1ce
vicedo19 <victorugochukwuka@gmail.com>
2023-06-20 06:38:32 -0700

    functions and nested loops directory added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
