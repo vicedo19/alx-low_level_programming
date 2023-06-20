627521d557d86d334e593a3d9335d2a170993612
vicedo19 <victorugochukwuka@gmail.com>
2023-06-20 16:33:09 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,8 @@ int print_last_digit(int n)
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
-		_putchar(last_digit + '0');
-		return (last_digit);
 	}
+	_putchar(last_digit + '0');
+	return (last_digit);
+	
 }
