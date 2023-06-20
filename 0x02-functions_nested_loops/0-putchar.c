6283ec650cf1a7686e7ba6578aee1ce8c726ad8e
vicedo19 <victorugochukwuka@gmail.com>
2023-06-20 07:29:11 -0700

    added a header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Prints "_putchar" followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
