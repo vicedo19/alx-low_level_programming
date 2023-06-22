28eb5ba268acabe41fb65ec79df8a52bc6f8efe4
vicedo19 <victorugochukwuka@gmail.com>
2023-06-22 15:27:39 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,18 @@
-/**
- * _isupper - check if c is upper
- *
- * @c: input for alphabet
- *
- * Return: 1 if its uppercase, 0 if not
-*/
+#include "main.h"
 
-int _isupper(int c)
+/**
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
+int _isdigit(int c)
 {
-    if (c >= 0 && c <= 9)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
