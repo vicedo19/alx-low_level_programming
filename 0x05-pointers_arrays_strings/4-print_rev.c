f36ab5b00f8d724c025c28fd0458efddc79c70ff
vicedo19 <victorugochukwuka@gmail.com>
2023-06-26 22:09:19 -0700

    2 - 9

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ *
+ * @s: input string.
+ *
+ * Return: no return.
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (c >= 0)
+	{
+		if (s[c] == '\0')
+		{
+			break;
+			c++;
+		}
+	}
+	for (c--; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
