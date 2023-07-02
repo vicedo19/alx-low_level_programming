9d0695fc3b3c36f5769abbf4fdc65e1685761b4a
vicedo19 <victorugochukwuka@gmail.com>
2023-07-02 14:40:41 -0700

    code updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -6,8 +7,8 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);
+	write(1, ch, strlen(ch));
 	return (1);
 }
