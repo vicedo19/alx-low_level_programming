453f95732d3172c02265b72419d32e4cc498acf8
vicedo19 <victorugochukwuka@gmail.com>
2023-07-05 13:35:15 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - used to write a string to stdout.
+ *
+ * @s: the string to be passed.
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s", s);
+}
