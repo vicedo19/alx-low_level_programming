3ea708ba2acdcf2713282d5dcd2e848920cd2c7c
vicedo19 <victorugochukwuka@gmail.com>
2023-07-05 15:49:59 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,11 @@
 
 int _strlen_recursion(char *s)
 {
+
 	/*Base condition*/
 	if (*s == '\0')
-	{
-		return (0);
-	{
+	return (0);
+
 	else
-	{
-		return (1 + _strlen_recursion(s + 1)); /* Sum 1*/
-	}
+	return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
 }
