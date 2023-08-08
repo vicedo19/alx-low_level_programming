0254fb591b6dc704490ab7147249888c905ca8a7
vicedo19 <victorugochukwuka@gmail.com>
2023-08-07 17:08:17 -0700

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
+	int i, y;
 	char *buf;
 
 	if (!filename)
-	{
 		return (0);
-	}
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-	{
+	if (fd < 0)
 		return (0);
-	}
-	buf = malloc(sizeof(char) * (letters));
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
 	{
+		free(buf);
 		return (0);
 	}
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	buf[i] = '\0';
 	close(fd);
-
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
 	free(buf);
-
-	return (nwr);
+	return (y);
 }
