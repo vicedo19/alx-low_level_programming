066edf52b561f01010f58378ec734fa921b0696c
vicedo19 <victorugochukwuka@gmail.com>
2023-09-24 10:58:03 -0700

    FizzBuzz code edited

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,28 @@
 int main(void)
 {
 	int num;
-
-	for (num = 1; num <= 100; ++num)
+	
+	num = 1;
+	while (num <= 100)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
+		if (num % 3 == 0)
+		{
 			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
+		}
+		else if (num % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if (num % 3 == 0 && num % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else
+		{
 			printf("%d", num);
-
-		if (num != 100)
-			printf(" ");
-		else
-			printf("\n");
+		}
+		printf(" ");
+		++i;
 	}
-	return (0);
+	printf("\n");
 }
