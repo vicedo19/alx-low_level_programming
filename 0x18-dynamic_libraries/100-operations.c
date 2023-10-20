de9e682bcde3be0cc45bb5c0e6de63974fc56b8f
Vicedo19 <victorugochukwuka@gmail.com>
2023-10-19 18:09:31 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - Computes the sum of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Computes the difference of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Computes the product of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Computes the quotient of two numbers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Computes the modulo of two numbers
+ * @a: The second number
+ * @b: The second number
+ *
+ * Return: Result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
